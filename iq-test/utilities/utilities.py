94c8b07cf5fd8ef2d669d2d70d7fcac76834494e
MGudik <MGudik@gmail.com>
2021-10-25 15:17:27 +0200

    Update utilities.py

--- iq-test/utilities/utilities.py
+++ iq-test/utilities/utilities.py
@@ -1,8 +1,29 @@
 
 import datetime
 import time
+from PIL import Image
+import BytesIO
+import base64
 
 start_time = time.time()
 
+
 def get_uptime():
     return '{}'.format(datetime.timedelta(seconds=time.time() - start_time))
+
+
+def decode_image(images):
+    if isinstance(images, list):
+        images = []
+        for image in images:
+            img = Image.open(BytesIO(base64.b64decode(images)))
+            img = img.convert('RGB')
+            images.append(img)
+        return images
+
+    elif isinstance(images, str):
+        img = Image.open(BytesIO(base64.b64decode(images)))
+        img = img.convert('RGB')
+        return img
+    else:
+        return images
